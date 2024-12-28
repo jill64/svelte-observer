fe766c1f8a238afbe324fad4057376dab0c0b4fb
jill64 <intents.turrets0h@icloud.com>
2024-12-28 09:44:48 +0900

    feat: enhance error handling in Sentry integration

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,7 +1,15 @@
 import { init } from '@jill64/sentry-sveltekit-cloudflare/client'
 
 const onError = init(
-  'https://7373b9c41f6f9b34df1d075ecd42ac5c@o4505814639312896.ingest.us.sentry.io/4508542758879232'
+  'https://7373b9c41f6f9b34df1d075ecd42ac5c@o4505814639312896.ingest.us.sentry.io/4508542758879232',
+  {
+    sentryOptions: {
+      beforeSend: (event) =>
+        (event.exception?.values?.[0].value ?? '') === 'Expected Error'
+          ? null
+          : event
+    }
+  }
 )
 
 export const handleError = onError()
