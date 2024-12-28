cc290e823c2735cc6b380e68bc1649ca52aeb2aa
jill64 <intents.turrets0h@icloud.com>
2024-12-28 09:27:36 +0900

    chore: update Vite configuration to use correct import for defineConfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite'
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // @ts-expect-error Invalid Types
   plugins: [sveltekit()],
   ssr: {
     noExternal: ['@jill64/sentry-sveltekit-cloudflare']
